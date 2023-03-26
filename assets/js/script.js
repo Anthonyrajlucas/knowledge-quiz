5732bcaf85ba3eeda8b6a5bb19f96f0ebdb1842e
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-03-26 14:06:37 +0000

    setup questions and options added questions will be asked

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// Questions will be asked
+const Questions = [{
+    id: 0,
+    q: "Which is the fastest bird in the world?",
+    a: [{ text: "Bald Eagle", isCorrect: false },
+        { text: "Peregrine Falcon", isCorrect: true },
+        { text: "Hummingbird", isCorrect: false },
+        { text: "Raven", isCorrect: false }
+    ]
+
+},
+{
+    id: 1,
+    q: "What is the tallest waterfall in the world?",
+    a: [{ text: "Angel Falls, Venezuela", isCorrect: true, isSelected: false },
+        { text: "Niagara Falls, New York", isCorrect: false },
+        { text: "Wailua Falls, Hawaii", isCorrect: false },
+        { text: "Sutherland Falls, New Zealand", isCorrect: false }
+    ]
+
+},
+{
+    id: 2,
+    q: "Which of these animals has the ability to laugh?",
+    a: [{ text: "Dog", isCorrect: false },
+        { text: "Whale", isCorrect: false },
+        { text: "Bear", isCorrect: false },
+        { text: "Rat", isCorrect: true }
+    ]
+
+},
+{
+    id: 3,
+    q: "Which color is not there in the rainbow?",
+    a: [{ text: "Indigo", isCorrect: false },
+        { text: "Red", isCorrect: false },
+        { text: "Brown", isCorrect: true },
+        { text: "Yellow", isCorrect: false }
+    ]
+
+},
+{
+    id: 4,
+    q: "Which is the longest river in the world?",
+    a: [{ text: "Mississippi", isCorrect: false },
+        { text: "Nile", isCorrect: true },
+        { text: "Amazon", isCorrect: false },
+        { text: "Yangtze", isCorrect: false }
+    ]
+
+}
+
+]
+
+
+
