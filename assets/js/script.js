f7cd653ee1177dba946bc95d52ce57bc40373ae2
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-03-27 15:12:13 +0000

    Add event listtener show selection for opitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,6 +87,42 @@ function iterate(id) {
 	op4.value = Questions[id].a[3].isCorrect;
 
 }
+var selected = "";
 
+	// Show selection for op1
+	op1.addEventListener("click", () => {
+		op1.style.backgroundColor = "lightgoldenrodyellow";
+		op2.style.backgroundColor = "lightskyblue";
+		op3.style.backgroundColor = "lightskyblue";
+		op4.style.backgroundColor = "lightskyblue";
+		selected = op1.value;
+	})
+
+	// Show selection for op2
+	op2.addEventListener("click", () => {
+		op1.style.backgroundColor = "lightskyblue";
+		op2.style.backgroundColor = "lightgoldenrodyellow";
+		op3.style.backgroundColor = "lightskyblue";
+		op4.style.backgroundColor = "lightskyblue";
+		selected = op2.value;
+	})
+
+	// Show selection for op3
+	op3.addEventListener("click", () => {
+		op1.style.backgroundColor = "lightskyblue";
+		op2.style.backgroundColor = "lightskyblue";
+		op3.style.backgroundColor = "lightgoldenrodyellow";
+		op4.style.backgroundColor = "lightskyblue";
+		selected = op3.value;
+	})
+
+	// Show selection for op4
+	op4.addEventListener("click", () => {
+		op1.style.backgroundColor = "lightskyblue";
+		op2.style.backgroundColor = "lightskyblue";
+		op3.style.backgroundColor = "lightskyblue";
+		op4.style.backgroundColor = "lightgoldenrodyellow";
+		selected = op4.value;
+	})
 
 
